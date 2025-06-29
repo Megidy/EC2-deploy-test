64568a745863f2dd00ac1769cfb67bb607f43a80
Megidy <byrka.sergiy@gmail.com>
2025-06-29 20:03:30 +0300

    fix: correct typo in Dockerfile and enhance main.go with signal handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	ec2test "github.com/Megidy/ec2-test"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+
+	cfg := ec2test.NewConifg()
+
+	dbPool := ec2test.NewPostgresConn(cfg.PostgresURI)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	server := ec2test.NewServer(cfg.HttpServerPort, dbPool, cfg.ApiKey)
+
+	err := server.Run()
+	if err != nil {
+		return
+	}
+
+	go func() {
+		<-sigs
+		log.Info().Msg("Received signal to shutdown")
+
+		err = server.Shutdown()
+		if err != nil {
+			log.Err(err).Msg("Failed to shutdown application")
+		}
+
+		err := server.Shutdown()
+		if err != nil {
+			log.Error().Err(err).Msg("failed to shutdown")
+		}
+	}()
+
+}
